ad9a3b701a8d0734f8f952836546d4c79b906b77
Shihabur Rahman Chowdhury <sr2chowdhury@uwaterloo.ca>
2014-02-06 23:14:56 -0500

    new file:   src/datastructure/lookup_table.h 	new file:   src/datastructure/lookup_table_iterator.h

---
+++ src/datastructure/lookup_table.h
@@ -0,0 +1,149 @@
+/*
+ * lookup_table.h
+ *
+ *  Created on: 2012-11-21
+ *      Author: sr2chowd
+ */
+
+#ifndef LOOKUP_TABLE_H
+#define LOOKUP_TABLE_H
+
+#include <map>
+#include <vector>
+#include <stdio.h>
+#include <pthread.h>
+
+using namespace std;
+
+template<class KeyType, class ValueType>
+class LookupTable {
+        map<KeyType, ValueType> table;
+        typename map<KeyType, ValueType>::iterator table_iterator;
+        pthread_rwlock_t table_lock;
+
+public:
+        LookupTable()
+		{
+                table.clear();
+                pthread_rwlock_init(&table_lock, NULL);
+        }
+
+        virtual ~LookupTable()
+        {
+                table.clear();
+                table_iterator = table.begin();
+                pthread_rwlock_destroy(&table_lock);
+        }
+
+        int size()
+        {
+                pthread_rwlock_rdlock(&table_lock);
+                int size = table.size();
+                pthread_rwlock_unlock(&table_lock);
+                return size;
+        }
+
+        void clear()
+        {
+                pthread_rwlock_wrlock(&table_lock);
+                table.clear();
+                pthread_rwlock_unlock(&table_lock);
+        }
+
+        bool add(KeyType& key, ValueType& value);
+        bool lookup(KeyType& key, ValueType& value);
+        bool update(KeyType& key, ValueType& value);
+        bool update_strict(KeyType& key, ValueType& value);
+        bool remove(KeyType& key);
+
+        typename map<KeyType, ValueType>::iterator begin()
+        {
+                return table.begin();
+        }
+
+        typename map<KeyType, ValueType>::iterator end()
+        {
+                return table.end();
+        }
+};
+
+template<typename KeyType, typename ValueType>
+bool LookupTable<KeyType, ValueType>::add(KeyType& key, ValueType& value)
+{
+        bool exists = false;
+
+        pthread_rwlock_wrlock(&table_lock);
+        if (table.find(key) == table.end())
+        {
+                exists = true;
+                table.insert(make_pair(key, value));
+        }
+        pthread_rwlock_unlock(&table_lock);
+        return !exists;
+}
+
+template<typename KeyType, typename ValueType>
+bool LookupTable<KeyType, ValueType>::lookup(KeyType& key, ValueType& value)
+{
+        bool success = false;
+
+        pthread_rwlock_rdlock(&table_lock);
+        if (table.find(key) != table.end())
+        {
+                success = true;
+                value = (*table.find(key)).second;
+        }
+        pthread_rwlock_unlock(&table_lock);
+        return success;
+}
+
+template<typename KeyType, typename ValueType>
+bool LookupTable<KeyType, ValueType>::update(KeyType& key, ValueType& value)
+{
+        bool exists = false;
+        pthread_rwlock_wrlock(&table_lock);
+        if (table.find(key) == table.end())
+        {
+                table.insert(make_pair(key, value));
+        }
+        else
+        {
+                table[key] = value;
+                exists = true;
+        }
+        pthread_rwlock_unlock(&table_lock);
+        return exists;
+}
+
+template<typename KeyType, typename ValueType>
+bool LookupTable<KeyType, ValueType>::update_strict(KeyType& key, ValueType& value)
+{
+        bool exists = false;
+        pthread_rwlock_wrlock(&table_lock);
+        if (table.find(key) == table.end())
+        {
+        	exists = false;
+        }
+        else
+        {
+                table[key] = value;
+                exists = true;
+        }
+        pthread_rwlock_unlock(&table_lock);
+        return exists;
+}
+
+template<typename KeyType, typename ValueType>
+bool LookupTable<KeyType, ValueType>::remove(KeyType& key)
+{
+        bool removed = false;
+        pthread_rwlock_wrlock(&table_lock);
+        if (table.find(key) != table.end())
+        {
+                table.erase(key);
+                removed = true;
+        }
+        pthread_rwlock_unlock(&table_lock);
+        return false;
+}
+#endif /* LOOKUP_TABLE_H_ */
---
+++ src/datastructure/lookup_table_iterator.h
@@ -0,0 +1,54 @@
+/*
+ * lookup_table_iterator.h
+ *
+ *  Created on: 2012-12-14
+ *      Author: sr2chowd
+ */
+#ifndef LOOKUP_ITERATOR_H
+#define LOOKUP_ITERATOR_H
+
+#include "lookup_table.h"
+#include <pthread.h>
+#include <map>
+
+using namespace std;
+
+template<class KeyType, class ValueType>
+class LookupTableIterator
+{
+	LookupTable<KeyType, ValueType>* table_ptr;
+	typename map<KeyType, ValueType>::iterator table_iterator;
+
+public:
+
+	LookupTableIterator()
+	{
+		table_ptr = NULL;
+	}
+	
+    LookupTableIterator(LookupTable<KeyType, ValueType>* ptr)
+	{
+		table_ptr = ptr;
+		table_iterator = table_ptr->begin();
+	}
+
+	void reset_iterator()
+	{
+		table_iterator = table_ptr->begin();
+	}
+
+	KeyType getNextKey()
+	{
+		KeyType ret = (*table_iterator).first;
+		table_iterator++;
+		return ret;
+	}
+
+	bool hasMoreKey()
+	{
+		return table_iterator != (table_ptr->end());
+	}
+};
+
+#endif
+
