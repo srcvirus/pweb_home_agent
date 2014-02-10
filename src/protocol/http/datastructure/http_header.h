4317d5f3ef00206b8745527e6cdba6e09ddcf31c
Shihabur Rahman Chowdhury <sr2chowdhury@uwaterloo.ca>
2014-02-10 14:08:53 -0500

    Adding files from boost example http server
    	new file:   src/protocol/http/datastructure/http_header.h
    	new file:   src/protocol/http/datastructure/http_reply.cpp
    	new file:   src/protocol/http/datastructure/http_reply.h
    	new file:   src/protocol/http/datastructure/http_request.h
    	new file:   src/protocol/http/http_request_parser.cpp
    	new file:   src/protocol/http/http_request_parser.h

---
+++ src/protocol/http/datastructure/http_header.h
@@ -0,0 +1,21 @@
+/*
+ * http_header.h
+ *
+ *  Created on: 2014-02-10
+ *      Author: sr2chowd
+ */
+
+#ifndef HTTP_HEADER_H_
+#define HTTP_HEADER_H_
+
+#include <string>
+
+using namespace std;
+
+struct header
+{
+	std::string name;
+	std::string value;
+};
+
+#endif /* HTTP_HEADER_H_ */
---
+++ src/protocol/http/datastructure/http_request.h
@@ -0,0 +1,33 @@
+/*
+ * http_request.h
+ *
+ *  Created on: 2014-02-10
+ *      Author: sr2chowd
+ */
+
+#ifndef HTTP_REQUEST_H_
+#define HTTP_REQUEST_H_
+
+// Copyright (c) 2003-2013 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <string>
+#include <vector>
+#include "http_header.h"
+
+using namespace std;
+
+/// A request received from a client.
+struct request
+{
+	std::string method;
+	std::string uri;
+	int http_version_major;
+	int http_version_minor;
+	std::vector<header> headers;
+};
+
+#endif /* HTTP_REQUEST_H_ */
